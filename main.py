947de3f43bd2a2f0f877f6555ebdbbc8bf182e9f
handriss <andrashinkel@gmail.com>
2016-08-09 22:27:26 +0200

    4 vertices rotating in 2d

--- main.py
+++ main.py
@@ -10,31 +10,56 @@ class Rect(object):
         self.height = height
         self.x = self.width/2
         self.y = self.height/2
-        self.canvas.create_oval(self.x, self.y, self.x+5, self.y+5, fill="blue")
 
         self.offset_x = 400
         self.offset_y = 400
 
-        node0 = [0, 100]
+        node0 = [-100, -100]
+        node1 = [100, 100]
+        node2 = [100, -100]
+        node3 = [-100, 100]
+        self.oval0 = self.canvas.create_oval(
+            self.offset_x+node0[0], self.offset_y+node0[1], self.offset_x+node0[0]+5,
+            self.offset_y+node0[1]+5, fill="red"
+        )
+        self.oval1 = self.canvas.create_oval(
+            100, 100, 105, 105, fill="red"
+        )
+        self.oval2 = self.canvas.create_oval(
+            100, 300, 105, 305, fill="red"
+        )
+        self.oval3 = self.canvas.create_oval(
+            300, 100, 305, 105, fill="red"
+        )
+        self.ova = self.canvas.create_oval(200, 200, 205, 205, fill="black")
+
+        self.coords = []
         for i in range(360):
-            self.rotate_z(1, node0)
+            time.sleep(0.005)
+            shift_x, shift_y = self.rotate_z(1, node0)
+            self.canvas.move(self.oval0, shift_x, shift_y)
+            x1, y1, x2, y2 = self.canvas.coords(self.oval0)
+
+            shift_x, shift_y = self.rotate_z(1, node1)
+            self.canvas.move(self.oval1, shift_x, shift_y)
+            shift_x, shift_y = self.rotate_z(1, node2)
+            self.canvas.move(self.oval2, shift_x, shift_y)
+            shift_x, shift_y = self.rotate_z(1, node3)
+            self.canvas.move(self.oval3, shift_x, shift_y)
+            self.canvas.update()
 
     def rotate_z(self, theta, node):
         sin_t = sin(radians(theta))
         cos_t = cos(radians(theta))
-
+        x_temp = node[0]
+        y_temp = node[1]
         x = node[0]
         y = node[1]
 
         node[0] = x * cos_t - y * sin_t
         node[1] = y * cos_t + x * sin_t
 
-        self.oval = self.canvas.create_oval(
-            self.offset_x+node[0], self.offset_x+node[1], self.offset_x+node[0]+5, self.offset_y+node[1]+5, fill="red"
-        )
-        time.sleep(0.025)
-        self.canvas.update()
-        self.canvas.delete('all')
+        return x_temp - node[0], y_temp - node[1]
 
 
 class App(object):
@@ -47,7 +72,7 @@ class App(object):
 
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
         self.canvas.pack()
-        # Create our Rect object:
+
         self.rect = Rect(self.canvas, 800, 800)
         self.root.mainloop()
 
