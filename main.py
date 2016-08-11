7025f8cf2115bb202dbdcb43bedce646fbcfc53b
handriss <andrashinkel@gmail.com>
2016-08-11 14:32:15 +0200

    4 vertices rotation clean coded, can be replaced by defining center

--- main.py
+++ main.py
@@ -6,60 +6,47 @@ import time
 class Rect(object):
     def __init__(self, canvas, width, height):
         self.canvas = canvas
-        self.width = width
-        self.height = height
-        self.x = self.width/2
-        self.y = self.height/2
 
-        self.offset_x = 400
-        self.offset_y = 400
+        center = [400, 400]
+        node0 = [100, 100]
+        node1 = [-100, 100]
+        node2 = [-100, -100]
+        node3 = [100, -100]
+        nodes = [node0, node1, node2, node3]
 
-        node0 = [-100, -100]
-        node1 = [100, 100]
-        node2 = [100, -100]
-        node3 = [-100, 100]
-        self.oval0 = self.canvas.create_oval(
-            self.offset_x+node0[0], self.offset_y+node0[1], self.offset_x+node0[0]+5,
-            self.offset_y+node0[1]+5, fill="red"
-        )
-        self.oval1 = self.canvas.create_oval(
-            100, 100, 105, 105, fill="red"
-        )
-        self.oval2 = self.canvas.create_oval(
-            100, 300, 105, 305, fill="red"
-        )
-        self.oval3 = self.canvas.create_oval(
-            300, 100, 305, 105, fill="red"
-        )
-        self.ova = self.canvas.create_oval(200, 200, 205, 205, fill="black")
+        vertices = []
+        edges = []
+        for node in nodes:
+            vertices.append(self.draw_circle(center[0]+node[0], center[1]+node[1], 5, fill="red"))
+
+        for i in range(len(nodes)):
+            edges.append(self.canvas.create_line(
+                nodes[i][0]+center[0], nodes[i][1]+center[1],
+                nodes[i-1][0]+center[0], nodes[i-1][1]+center[1], fill="black"
+            ))
 
         self.coords = []
         for i in range(360):
-            time.sleep(0.005)
-            shift_x, shift_y = self.rotate_z(1, node0)
-            self.canvas.move(self.oval0, shift_x, shift_y)
-            x1, y1, x2, y2 = self.canvas.coords(self.oval0)
+            time.sleep(0.025)
 
-            shift_x, shift_y = self.rotate_z(1, node1)
-            self.canvas.move(self.oval1, shift_x, shift_y)
-            shift_x, shift_y = self.rotate_z(1, node2)
-            self.canvas.move(self.oval2, shift_x, shift_y)
-            shift_x, shift_y = self.rotate_z(1, node3)
-            self.canvas.move(self.oval3, shift_x, shift_y)
-            self.canvas.update()
+            for node, vortex in zip(nodes, vertices):
+                shift_x, shift_y = self.rotate_z(1, node)
+                self.canvas.move(vortex, shift_x, shift_y)
+                self.canvas.update()
+
+    def draw_circle(self, x, y, size, **kwargs):
+        return self.canvas.create_oval(x-size/2, y-size/2, x+size/2, y+size/2, **kwargs)
 
     def rotate_z(self, theta, node):
-        sin_t = sin(radians(theta))
-        cos_t = cos(radians(theta))
         x_temp = node[0]
         y_temp = node[1]
         x = node[0]
         y = node[1]
 
-        node[0] = x * cos_t - y * sin_t
-        node[1] = y * cos_t + x * sin_t
+        node[0] = x * cos(radians(theta)) - y * sin(radians(theta))
+        node[1] = y * cos(radians(theta)) + x * sin(radians(theta))
 
-        return x_temp - node[0], y_temp - node[1]
+        return -(x_temp - node[0]), -(y_temp - node[1])
 
 
 class App(object):
